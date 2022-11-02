f97715600a95e8e2ebc45edfd50255f187a74c45
neil <mallarineil9@gmail.com>
2022-11-02 02:12:17 -0400

    Rename variables

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const taskNameTextarea = document.querySelector('#task-name');
+const taskNameTextareaElem = document.querySelector('#task-name');
 
-const addTaskButtons = document.querySelectorAll('.add-task-button');
-addTaskButtons.forEach(addTaskButton => {
-    addTaskButton.addEventListener('click', () => {
-        const taskModal = document.querySelector('#task-modal');
-        taskModal.showModal();
+const addTaskButtonElems = document.querySelectorAll('.add-task-button');
+addTaskButtonElems.forEach(addTaskButtonElem => {
+    addTaskButtonElem.addEventListener('click', () => {
+        const taskModalElem = document.querySelector('#task-modal');
+        taskModalElem.showModal();
 
-        if (!taskNameTextarea.hasAttribute('required')) {
-            taskNameTextarea.setAttribute('required', '');
+        if (!taskNameTextareaElem.hasAttribute('required')) {
+            taskNameTextareaElem.setAttribute('required', '');
         }
     });
 });
 
-const taskModalCloseButton = document.querySelector('#close-task-modal-button');
-taskModalCloseButton.addEventListener('click', () => {
+const taskModalCloseButtonElem = document.querySelector('#close-task-modal-button');
+taskModalCloseButtonElem.addEventListener('click', () => {
     // this allows the task modal from closing
-    taskNameTextarea.removeAttribute('required');
+    taskNameTextareaElem.removeAttribute('required');
 });
