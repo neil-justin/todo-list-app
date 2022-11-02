f190ffe626dda2f8269f8bef6bc882453ca567ef
neil <mallarineil9@gmail.com>
2022-11-02 01:47:22 -0400

    Make the task modal's close button work as intended

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-const addTaskButtons = document.querySelectorAll('.add-task-button');
+const taskNameTextarea = document.querySelector('#task-name');
 
+const addTaskButtons = document.querySelectorAll('.add-task-button');
 addTaskButtons.forEach(addTaskButton => {
     addTaskButton.addEventListener('click', () => {
         const taskModal = document.querySelector('#task-modal');
         taskModal.showModal();
+
+        if (!taskNameTextarea.hasAttribute('required')) {
+            taskNameTextarea.setAttribute('required', '');
+        }
     });
-})
+});
+
+const taskModalCloseButton = document.querySelector('#close-task-modal-button');
+taskModalCloseButton.addEventListener('click', () => {
+    // this allows the task modal from closing
+    taskNameTextarea.removeAttribute('required');
+});
